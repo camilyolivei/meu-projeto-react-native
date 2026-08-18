5cff7d73af40923cf7134c20e0f1ee6009e63578
camilyolivei <camilyoliveira.1gui@gmail.com>
2026-08-18 19:33:00 -0300

    feat: Exercicio 2 completo

--- exercicio02/App.tsx
+++ exercicio02/App.tsx
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  ImageBackground,
+  Pressable,
+} from 'react-native';
+
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+} from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.backGroundTelaInteira}> 
+
+        <ImageBackground
+          source={{
+            uri: 'https://www.petlove.com.br/images/breeds/227696/profile/original/perfil-coelho.jpg?1597789099', width: '100%', height: '100%'
+          }}
+          style={styles.backGroundTelaInteira}  
+        >
+          <Text>Nome:</Text>
+          <TextInput />
+
+          <Text>Senha:</Text>
+          <TextInput />
+
+          <Pressable>
+            <Text>Enviar</Text>
+          </Pressable>
+
+        </ImageBackground>
+
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  backGroundTelaInteira: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
